35838d82c4f5a66118649d8a29616d40aea5c36f
bobek <bobbudowniczy123@gmail.com>
2022-09-30 08:13:29 +0200

    o

--- Programowanie strukturalne i obiektowe/Cordinates/Cordinates.cpp
+++ Programowanie strukturalne i obiektowe/Cordinates/Cordinates.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
+struct point
+{
+	int x;
+	int y;
 
+};
 void cordinateTestVersion1()
 {
 	int x, y;
@@ -9,6 +14,17 @@ void cordinateTestVersion1()
 	cin >> x;
 	cout << "Podaj y\n";
 	cin >> y;
+	double distance = sqrt(x*x+y*y);
+	cout << distance;
+}
+void cordinateTestVersion2()
+{
+	int x, y;
+	point point;
+	cout << "Podaj x\n";
+	cin >> point.x;
+	cout << "Podaj y\n";
+	cin >> point.y;
 	double distance = sqrt(x*x+y*y);
 	cout << distance;
 }
